29d149f44707a21408fb5073a2d35a7565dd0fe8
powerboat9 <powerboat9@users.noreply.github.com>
2017-06-01 21:41:55 -0400

    *Finished* transmission code

--- lib.js
+++ lib.js
@@ -1,4 +1,63 @@
-window.UDT = {};
-window.UDT.raw = {
-    tone : function(freq, dur, amp) {
-        
+(function() {
+    var aud = window.audioContext || window.webkitAudioContext;
+    function tone(freq, dur, amp, c) {
+        c = c || function() {};
+        var o = aud.createOscilator();
+        var g = aud.createGain();
+        o.connect(g);
+        g.connect(aud.destination);
+        g.gain.value = amp / 100;
+        o.frequency.value = freq;
+        o.type = 0;
+        o.start();
+        setTimeout(function() {
+            o.stop();
+            c();
+        }, dur);
+    }
+    var isTransmiting = false;
+    function transmit(data, bins, freqMin, freqMax, toneDur, v) { // data is a normal string, bin is in bits, LSB
+        isTransmiting = true;
+        v = v || 1;
+        var writeData = [];
+        do {
+            var base = 1;
+            var writePos = 0;
+            var writeN = 0;
+            for (let i = 0; i < data.length; i++) {
+                var v = data.charCodeAt(i);
+                while (true) {
+                    if (v & base) {
+                        writeN += (writePos ** 2);
+                    }
+                    writePos++;
+                    if (writePos == bins) {
+                        writePos = 0;
+                        writeData.push(writeN);
+                        writeN = 0;
+                    }
+                    base = base << 1;
+                    if (base == 256) {
+                        base = 1;
+                        break;
+                    }
+                }
+            }
+        }
+        var i = 0;
+        function a() {
+            if (isTransmiting) {
+                tone(writeData[i], toneDur, v, (i == writeData.length) ? (isTransmiting = false, function() {}) : a);
+                i++;
+            }
+        }
+        a();
+    }
+    function emergencyTransmitFree() {
+        isTransmiting = false;
+    }
+    window.UDT = {
+        transmit : transmit,
+        emergencyTransmitFree : emergencyTransmitFree
+    }
+})()
